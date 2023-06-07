26bcb959e19c87ce3d4c759da20aed4c2f12e486
Callum <35613@ma-web.nl>
2023-06-07 15:21:56 +0200

    25%

---
+++ filter.js
@@ -0,0 +1,143 @@
+let allGames = document.getElementsByClassName("game")
+
+let products = document.getElementsByClassName("products");
+
+let filters = document.getElementsByClassName("filter");
+
+let Youtubers = document.getElementById("Youtubers-checkbox")
+
+console.log(products);
+
+window.scrollTo(0,0);
+
+setInterval(function(){
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+},1500);
+
+
+for(let i = 0; i < filters.length; i++){
+    filters[i].checked = true;
+}
+
+Youtubers.onchange = function(){ 
+    if(Youtubers.checked === true){
+        for(let i = 0; i < products.length; i++){
+            if(products[i].dataset.category === 'Youtubers'){
+                products[i].style.display = "block";
+            }
+        }
+    }
+    else{
+        for(let i = 0; i < products.length; i++){
+            if(products[i].dataset.category === 'Youtubers'){
+                products[i].style.display = "none";
+            }
+        }
+
+    }
+}
+
+
+
+let TvShows = document.getElementById("checkbox-TVShow")
+
+TvShows.onchange = function(){
+    if(TvShows.checked === true){
+        for(let i = 0; i <  products.length; i++){
+            if( products[i].dataset.category === 'TVShows'){
+                products[i].style.display = "block";
+            }
+        }
+    }
+    else{
+        for(let i = 0; i <  products.length; i++){
+            if( products[i].dataset.category === 'TVShows'){
+                products[i].style.display = "none";
+            }
+        }
+    
+    }
+    }
+
+
+let game = document.getElementById("checkbox-Games")
+
+game.onchange = function(){
+    if(game.checked === true){
+        for(let i = 0; i <  products.length; i++){
+            if( products[i].dataset.category === 'Games'){
+                products[i].style.display = "block";
+            }
+        }
+    }
+    else{
+        for(let i = 0; i <  products.length; i++){
+            if( products[i].dataset.category === 'Games'){
+                products[i].style.display = "none";
+            }
+        }
+    
+    }
+    }
+
+
+
+
+
+    const addToCartButtons = document.getElementsByClassName("games__button");
+    let cartMessage = 0;
+    const cartMessageValue = document.getElementsByClassName("shoppingCart__message")[0];
+    const shoppingModal = document.getElementById("shoppingModal");
+    let modalIsOpen = false;
+    let buzz = 0;
+    let iron = 0;
+    let c3po = 0;
+    
+    for(let i = 0; i < addToCartButtons.length; i++){
+        addToCartButtons[i].onclick = function(){
+            cartMessage += 1;
+            cartMessageValue.innerHTML = cartMessage
+            switch(addToCartButtons[i].dataset.product){
+                case "Buzz":
+                    buzz+= 1;
+                    break
+                case "ironman":
+                    iron+= 1;
+                break
+                case "C3po":
+                    c3po+= 1;
+                    break
+            }
+            if(modalIsOpen === false){
+                shoppingModal.style.display = "flex";
+                modalIsOpen = true;
+                setTimeout(function(){
+                    shoppingModal.style.display = "none";
+                    modalIsOpen = false;
+                },2000);
+            }
+          
+        } 
+    }
+    
+    const checkoutButton = document.getElementById("checkout__button");
+    const checkoutWindow = document.getElementById("js--checkoutWindow")
+    let checkoutIsOpen = false;
+    
+    checkoutButton.onclick = function(){
+        if(checkoutIsOpen === false){
+            document.querySelector("main").style.display = "none";
+            checkoutWindow.style.display = "block";
+            checkoutIsOpen = true;
+            document.getElementById("js--amount-buzz").innerHTML = buzz + "x";
+            document.getElementById("js--amount-ironman").innerHTML = iron + "x";
+            document.getElementById("js--amount-c3po").innerHTML = c3po + "x";
+            return;
+        }
+        document.querySelector("main").style.display = "block";
+        checkoutWindow.style.display = "none";
+        checkoutIsOpen = false;
+    }
+    
+    
+    
