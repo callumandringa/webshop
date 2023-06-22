be7018389a94db260b5fb452900f0e5c7305c32a
Callum <35613@ma-web.nl>
2023-06-22 12:22:18 +0200

    fotos bijgefoegd

---
+++ modal.js
@@ -0,0 +1,21 @@
+const medium = document.getElementById("rondjes-medium")
+const large = document.getElementById("rondjes-large")
+const xxl = document.getElementById("rondjes-xxl")
+const image = document.getElementById("image-bigger")
+
+medium.onclick = function(){
+    image.style.scale = "1"
+
+}
+
+
+large.onclick = function(){
+    image.style.scale = "1.2"
+
+}
+
+
+xxl.onclick = function(){
+    image.style.scale = "1.3"
+
+}
